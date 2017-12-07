d545669731de2d11025394f1a20eec72cffc99ba
pixelherodev <pixelherodev@outlook.com>
2017-12-07 13:10:54 -0500

    Finish up failed experiment.
    Going to have to make each page separately and concactenate them
    together.

--- include/system.h
+++ include/system.h
@@ -1,3 +1,5 @@
+#pragma once
+
 void halt();
 void reset();
 void shutdown();
@@ -7,9 +9,19 @@ void swapBanks(char bank,char index);
 void memcpy(char * dest, char * src,int length);
 void memfill(char *dest,char val,int length);
 void panic(char *what);
+unsigned char getPageFromAddress(int address);
+
+// Must be used at way beginning of function - make sure nothing is on stack
+void * getCaller();
+
+typedef void (*function)();
 
 #define TITLE(x) void title() __naked {                                        \
 	__asm                                                                      \
 	.db x, 0                                                                   \
 	__endasm;                                                                  \
 }
+
+function PAGED_CALL(function func,unsigned char page);
+void * PAGED_ADDRESS(int address);
+#define PAGED_POINTER(x) PAGED_ADDRESS((int)x)
--- src/system.c
+++ src/system.c
@@ -73,6 +73,7 @@ short _swapBanks(char bank,char index) __naked{
 	push af
 	ld a, 3
 	out (2), a
+	pop af
 	ret
 	__endasm;
 	bank;index;
@@ -85,9 +86,61 @@ void swapBanks(char bank, char index){
 	}
 }
 
+void * getCaller() __naked {
+	__asm
+	pop af
+	pop hl
+	push hl
+	push af
+	ret
+	__endasm;
+}
+
+unsigned char getPageFromAddress(int page){
+	if (page >= 0xC000) {
+		return 3;
+	}
+	else if (page >= 0x8000) {
+		return 2;
+	}
+	else if (page >= 0x4000) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+function PAGED_CALL(function func,unsigned char page){
+	int i;
+	if (page > 3) {
+		panic("Attempted to call a function on a non existent page!");
+		return reset;
+	}
+	 i = (int)func;
+	 i &= 0x3FFF;
+	 i |= 0x4000 * page;
+	 return (function)i;
+}
+
+void * PAGED_ADDRESS(int ptr){
+	// MUST NOT HAVE TOO MANY LOCAL VARS OR CALLER IS THROWN OFF!
+	void * caller = getCaller();
+	{
+		unsigned char page = getPageFromAddress((int)caller);
+		if (page > 3) {
+			panic("Attempted to access a non existent page!");
+			return reset;
+		}
+		ptr &= 0x3FFF;
+		ptr |= 0x4000 * page;
+		return (void*)ptr;
+	}
+}
+
 void panic(char *what){
 	cls();
 	puts(what);
-	puts("\n\nTo ignore this error, right click anywhere in the emulator");
+	puts("\n\nTo ignore this error, right click anywhere in the emulator \n(Not recommended)");
 	halt();
 }
